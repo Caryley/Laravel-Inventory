f04672ee3b6a387e25691a4e8410aace95f84e59
talelmishali <talelmishali@gmail.com>
2021-02-04 10:12:36 -0500

    -Add to a non existing inventory
    -Now can add to a non existing inventory, which will create an inventory and add the given quantity.
    -Added documentation.

--- src/Exeptions/InvalidInventoryModel.php
+++ src/Exeptions/InvalidInventoryModel.php
@@ -6,7 +6,13 @@ use Exception;
 
 class InvalidInventoryModel extends Exception
 {
-    public static function create(string $model): self
+    /**
+     * Invalid inventory model exception for an invalid model passed which does not extends \Caryley\LaravelInventory\Inventory.
+     *
+     * @param  string $model
+     * @return self
+     */
+    public static function create($model)
     {
         return new self("The model `{$model}` is invalid. A valid model must extend the model Caryley\LaravelInventory\Inventory.");
     }
